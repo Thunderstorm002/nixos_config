d5b85e32d665f447dac812816d0a072614703e79
roshan <roshan.nair@protonmail.com>
2025-07-21 23:37:30 +0530

    custom changes

--- home/config/rofi.nix
+++ home/config/rofi.nix
@@ -5,7 +5,7 @@
     theme = "${config.xdg.configHome}/rofi/themes/rofi-themes-collection/themes/rounded-blue-dark.rasi";
     extraConfig = {
       modi = "drun,run,window";
-      font = "0xproto 10";
+      font = "FiraCode Nerd Font Med 10";
       show-icons = true;
       icon-theme = "Papirus";
     };
