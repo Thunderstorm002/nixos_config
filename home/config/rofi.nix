227e0b79aefd4908712330f86083435ddcc1d150
roshan <roshan.nair@protonmail.com>
2025-07-23 03:01:07 +0530

    custom changes

--- home/config/rofi.nix
+++ home/config/rofi.nix
@@ -2,9 +2,8 @@
 {
   programs.rofi = {
     enable = true;
-    theme = "${config.xdg.configHome}/rofi/themes/themes/Monokai.rasi";
+    theme = "${config.xdg.configHome}/rofi/themes/themes/gruvbox-dark-hard.rasi";
     extraConfig = {
-      font = "FiraCode Nerd Font Med 10";
       show-icons = true;
       icon-theme = "Papirus";
     };
