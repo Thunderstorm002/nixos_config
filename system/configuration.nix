9b784b31bb2201c14eacc23b707cd5dd6388e87d
roshan <roshan.nair@protonmail.com>
2025-07-24 17:16:33 +0530

    custom changes

--- system/configuration.nix
+++ system/configuration.nix
@@ -67,17 +67,17 @@
     mode = "440";
   };
 
-  # Tailscale
-  services.tailscale = {
-    enable = true;
-    openFirewall = true;
-    authKeyFile = config.age.secrets.vpn-preauth.path;
-    extraUpFlags = [
-      #"--login-server=https://your-instance" # if you use a non-default tailscale coordinator
-      "--accept-routes"
-      "--accept-dns=false" # if its' a server you prolly dont need magicdns
-    ];
-  };
+  #  # Tailscale
+  #  services.tailscale = {
+  #    enable = true;
+  #    openFirewall = true;
+  #    authKeyFile = config.age.secrets.vpn-preauth.path;
+  #    extraUpFlags = [
+  #      #"--login-server=https://your-instance" # if you use a non-default tailscale coordinator
+  #      "--accept-routes"
+  #      "--accept-dns=false" # if its' a server you prolly dont need magicdns
+  #    ];
+  #  };
 
   # Virtualization with Podman
   virtualisation.containers.enable = true;
@@ -182,6 +182,9 @@
     upower
     libnotify
     util-linux
+
+    #vpn
+    tailscale
 
     #Security
     sops
