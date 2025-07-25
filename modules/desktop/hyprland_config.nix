9003d4e9ba1cf69bb9568ca57753baa8a1a061fb
roshan <roshan.nair@protonmail.com>
2025-07-25 21:22:18 +0530

    custom changes

--- modules/desktop/hyprland_config.nix
+++ modules/desktop/hyprland_config.nix
@@ -7,18 +7,6 @@
 
 {
 
-  # Desktop Environment
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-  };
-
-  # Disable SDDM
-  services.displayManager.sddm.enable = false;
-  # Ensure the system boots into a multi-user target (text mode)
-  systemd.services.display-manager.enable = false;
-
   # Enable Hyprland
   wayland.windowManager.hyprland = {
     enable = true;
