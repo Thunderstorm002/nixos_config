d7d2c2dc098b654bb134f5bccbca898ebd4a2b85
roshan <roshan.nair@protonmail.com>
2025-07-28 11:37:50 +0530

    custom changes

--- home/config/hyprland_config.nix
+++ home/config/hyprland_config.nix
@@ -196,7 +196,7 @@
         "$mainMod SHIFT, 8, movetoworkspace, 8"
         "$mainMod SHIFT, 9, movetoworkspace, 9"
         "$mainMod SHIFT, 0, movetoworkspace, 10"
-        "$mainMod, S, togglespecialworkspace, magic"
+        "$mainMod, G, togglespecialworkspace, magic"
         "$mainMod SHIFT, S, movetoworkspace, special:magic"
         "$mainMod, mouse_down, workspace, e+1"
         "$mainMod, mouse_up, workspace, e-1"
