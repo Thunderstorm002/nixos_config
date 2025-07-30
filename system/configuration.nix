a3e49d7bdce08a1f8617fe2f93566d565531aa18
roshan <roshan.nair@protonmail.com>
2025-07-30 17:33:18 +0530

    custom changes

--- system/configuration.nix
+++ system/configuration.nix
@@ -233,7 +233,7 @@
       #     they're being parsed in nix, which lacks unicode
       #     support.
       # config = ./emacs.org;
-      config = ../doomemacs/early-init.el;
+      config = "/home/roshan/nixos_config/doomemacs";
 
       # Whether to include your config as a default init file.
       # If being bool, the value of config is used.
