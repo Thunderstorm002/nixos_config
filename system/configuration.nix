39a31fd26a799fdcacf60759eea73a6c0a49d01d
roshan <roshan.nair@protonmail.com>
2025-08-12 15:55:15 +0530

    custom changes

--- system/configuration.nix
+++ system/configuration.nix
@@ -170,14 +170,6 @@
     # Keyboard
     kanata
 
-    # nixCats
-      (nixCats.utils.forSystem "x86_64-linux").makeNvim {
-        name = "nvim";
-        settings = { wrapRc = true; configDir = ../config/neovim/.; };
-        categoryDefinitions = { startupPlugins = [ "vim-nix" ]; };
-        packageDefinitions = { nvim = { startupPlugins.vim-nix = true; }; };
-      }
-
     #Emacs
     #(pkgs.emacsWithPackagesFromUsePackage {
     #  # Your Emacs config file. Org mode babel files are also
