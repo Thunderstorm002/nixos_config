58281b5ef15794287802b5e2678c21a3727807cf
roshan <roshan.nair@protonmail.com>
2025-08-13 22:48:59 +0530

    custom changes

--- modules/theme/stylix.nix
+++ modules/theme/stylix.nix
@@ -6,7 +6,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     fonts = {
       serif = {
         package = pkgs.dejavu_fonts;
