f819a516d38267dff05be8fdcb7f1d018378b751
roshan <roshan.nair@protonmail.com>
2025-08-13 15:07:38 +0530

    custom changes

--- home/config/doom/config.el
+++ home/config/doom/config.el
@@ -131,104 +131,104 @@
   ;; Additional configurations can go here
   )
 
-(after! org
-  (add-to-list 'org-modules 'org-habit)
-  (setq org-startup-folded 'show2levels
-        org-ellipsis " [...] "
-        org-capture-templates
-        '(("t" "todo" entry (file+headline "todo.org" "Inbox")
-           "* [ ] %?\n%i\n%a"
-           :prepend t)
-          ("d" "deadline" entry (file+headline "todo.org" "Inbox")
-           "* [ ] %?\nDEADLINE: <%(org-read-date)>\n\n%i\n%a"
-           :prepend t)
-          ("s" "schedule" entry (file+headline "todo.org" "Inbox")
-           "* [ ] %?\nSCHEDULED: <%(org-read-date)>\n\n%i\n%a"
-           :prepend t)
-          ("c" "check out later" entry (file+headline "todo.org" "Check out later")
-           "* [ ] %?\n%i\n%a"
-           :prepend t)
-          ("l" "ledger" plain (file "ledger/personal.gpg")
-           "%(+beancount/clone-transaction)"))))
-
-(after! org-agenda
-  (setq org-agenda-todo-list-sublevels nil
-        org-agenda-compact-blocks t
-        org-agenda-sorting-strategy
-        '((agenda time-up category-keep habit-up priority-down)
-          (todo priority-down category-keep) (tags priority-down category-keep)
-          (search category-keep))))
-
-(after! org-roam
-  (setq org-roam-capture-templates
-        `(("n" "note" plain
-           ,(format "#+title: ${title}\n%%[%s/template/note.org]" org-roam-directory)
-           :target (file "note/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("r" "thought" plain
-           ,(format "#+title: ${title}\n%%[%s/template/thought.org]" org-roam-directory)
-           :target (file "thought/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("t" "topic" plain
-           ,(format "#+title: ${title}\n%%[%s/template/topic.org]" org-roam-directory)
-           :target (file "topic/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("c" "contact" plain
-           ,(format "#+title: ${title}\n%%[%s/template/contact.org]" org-roam-directory)
-           :target (file "contact/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("p" "project" plain
-           ,(format "#+title: ${title}\n%%[%s/template/project.org]" org-roam-directory)
-           :target (file "project/%<%Y%m%d>-${slug}.org")
-           :unnarrowed t)
-          ("i" "invoice" plain
-           ,(format "#+title: %%<%%Y%%m%%d>-${title}\n%%[%s/template/invoice.org]" org-roam-directory)
-           :target (file "invoice/%<%Y%m%d>-${slug}.org")
-           :unnarrowed t)
-          ("f" "ref" plain
-           ,(format "#+title: ${title}\n%%[%s/template/ref.org]" org-roam-directory)
-           :target (file "ref/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("w" "works" plain
-           ,(format "#+title: ${title}\n%%[%s/template/works.org]" org-roam-directory)
-           :target (file "works/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("s" "secret" plain "#+title: ${title}\n\n"
-           :target (file "secret/%<%Y%m%d%H%M%S>-${slug}.org.gpg")
-           :unnarrowed t))
-        ;; Use human readable dates for dailies titles
-        org-roam-dailies-capture-templates
-        `(("d" "default" plain ""
-           :target (file+head "%<%Y-%m-%d>.org" ,(format "%%[%s/template/journal.org]" org-roam-directory))))))
-
-(after! org-tree-slide
-  ;; I use g{h,j,k} to traverse headings and TAB to toggle their visibility, and
-  ;; leave C-left/C-right to .  I'll do a lot of movement because my
-  ;; presentations tend not to be very linear.
-  (setq org-tree-slide-skip-outline-level 2))
-
-(after! org-roam
-  ;; Offer completion for #tags and @areas separately from notes.
-  (add-to-list 'org-roam-completion-functions #'org-roam-complete-tag-at-point)
-
-  ;; Automatically update the slug in the filename when #+title: has changed.
-  (add-hook 'org-roam-find-file-hook #'org-roam-update-slug-on-save-h)
-
-  ;; Make the backlinks buffer easier to peruse by folding leaves by default.
-  (add-hook 'org-roam-buffer-postrender-functions #'magit-section-show-level-2)
-
-  ;; List dailies and zettels separately in the backlinks buffer.
-  (advice-add #'org-roam-backlinks-section :override #'org-roam-grouped-backlinks-section)
-
-  ;; Open in focused buffer, despite popups
-  (advice-add #'org-roam-node-visit :around #'+popup-save-a)
-
-  ;; Make sure tags in vertico are sorted by insertion order, instead of
-  ;; arbitrarily (due to the use of group_concat in the underlying SQL query).
-  (advice-add #'org-roam-node-list :filter-return #'org-roam-restore-insertion-order-for-tags-a)
-
-  ;; Add ID, Type, Tags, and Aliases to top of backlinks buffer.
-  (advice-add #'org-roam-buffer-set-header-line-format :after #'org-roam-add-preamble-a))
+;;(after! org
+;;  (add-to-list 'org-modules 'org-habit)
+;;  (setq org-startup-folded 'show2levels
+;;        org-ellipsis " [...] "
+;;        org-capture-templates
+;;        '(("t" "todo" entry (file+headline "todo.org" "Inbox")
+;;           "* [ ] %?\n%i\n%a"
+;;           :prepend t)
+;;          ("d" "deadline" entry (file+headline "todo.org" "Inbox")
+;;           "* [ ] %?\nDEADLINE: <%(org-read-date)>\n\n%i\n%a"
+;;           :prepend t)
+;;          ("s" "schedule" entry (file+headline "todo.org" "Inbox")
+;;           "* [ ] %?\nSCHEDULED: <%(org-read-date)>\n\n%i\n%a"
+;;           :prepend t)
+;;          ("c" "check out later" entry (file+headline "todo.org" "Check out later")
+;;           "* [ ] %?\n%i\n%a"
+;;           :prepend t)
+;;          ("l" "ledger" plain (file "ledger/personal.gpg")
+;;           "%(+beancount/clone-transaction)"))))
+;;
+;;(after! org-agenda
+;;  (setq org-agenda-todo-list-sublevels nil
+;;        org-agenda-compact-blocks t
+;;        org-agenda-sorting-strategy
+;;        '((agenda time-up category-keep habit-up priority-down)
+;;          (todo priority-down category-keep) (tags priority-down category-keep)
+;;          (search category-keep))))
+;;
+;;(after! org-roam
+;;  (setq org-roam-capture-templates
+;;        `(("n" "note" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/note.org]" org-roam-directory)
+;;           :target (file "note/%<%Y%m%d%H%M%S>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("r" "thought" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/thought.org]" org-roam-directory)
+;;           :target (file "thought/%<%Y%m%d%H%M%S>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("t" "topic" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/topic.org]" org-roam-directory)
+;;           :target (file "topic/%<%Y%m%d%H%M%S>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("c" "contact" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/contact.org]" org-roam-directory)
+;;           :target (file "contact/%<%Y%m%d%H%M%S>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("p" "project" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/project.org]" org-roam-directory)
+;;           :target (file "project/%<%Y%m%d>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("i" "invoice" plain
+;;           ,(format "#+title: %%<%%Y%%m%%d>-${title}\n%%[%s/template/invoice.org]" org-roam-directory)
+;;           :target (file "invoice/%<%Y%m%d>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("f" "ref" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/ref.org]" org-roam-directory)
+;;           :target (file "ref/%<%Y%m%d%H%M%S>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("w" "works" plain
+;;           ,(format "#+title: ${title}\n%%[%s/template/works.org]" org-roam-directory)
+;;           :target (file "works/%<%Y%m%d%H%M%S>-${slug}.org")
+;;           :unnarrowed t)
+;;          ("s" "secret" plain "#+title: ${title}\n\n"
+;;           :target (file "secret/%<%Y%m%d%H%M%S>-${slug}.org.gpg")
+;;           :unnarrowed t))
+;;        ;; Use human readable dates for dailies titles
+;;        org-roam-dailies-capture-templates
+;;        `(("d" "default" plain ""
+;;           :target (file+head "%<%Y-%m-%d>.org" ,(format "%%[%s/template/journal.org]" org-roam-directory))))))
+;;
+;;(after! org-tree-slide
+;;  ;; I use g{h,j,k} to traverse headings and TAB to toggle their visibility, and
+;;  ;; leave C-left/C-right to .  I'll do a lot of movement because my
+;;  ;; presentations tend not to be very linear.
+;;  (setq org-tree-slide-skip-outline-level 2))
+;;
+;;(after! org-roam
+;;  ;; Offer completion for #tags and @areas separately from notes.
+;;  (add-to-list 'org-roam-completion-functions #'org-roam-complete-tag-at-point)
+;;
+;;  ;; Automatically update the slug in the filename when #+title: has changed.
+;;  (add-hook 'org-roam-find-file-hook #'org-roam-update-slug-on-save-h)
+;;
+;;  ;; Make the backlinks buffer easier to peruse by folding leaves by default.
+;;  (add-hook 'org-roam-buffer-postrender-functions #'magit-section-show-level-2)
+;;
+;;  ;; List dailies and zettels separately in the backlinks buffer.
+;;  (advice-add #'org-roam-backlinks-section :override #'org-roam-grouped-backlinks-section)
+;;
+;;  ;; Open in focused buffer, despite popups
+;;  (advice-add #'org-roam-node-visit :around #'+popup-save-a)
+;;
+;;  ;; Make sure tags in vertico are sorted by insertion order, instead of
+;;  ;; arbitrarily (due to the use of group_concat in the underlying SQL query).
+;;  (advice-add #'org-roam-node-list :filter-return #'org-roam-restore-insertion-order-for-tags-a)
+;;
+;;  ;; Add ID, Type, Tags, and Aliases to top of backlinks buffer.
+;;  (advice-add #'org-roam-buffer-set-header-line-format :after #'org-roam-add-preamble-a))
 
 
 ;;; :ui doom-dashboard
@@ -238,14 +238,14 @@
 
 
 ;;; :app everywhere
-(after! emacs-everywhere
-  ;; Easier to match with a bspwm rule:
-  ;;   bspc rule -a 'Emacs:emacs-everywhere' state=floating sticky=on
-  (setq emacs-everywhere-frame-name-format "emacs-anywhere")
-
-  ;; Leave it to BSPWM to position/size the emacs-everywhere window, rather than
-  ;; place it at the cursor position (which could be anywhere).
-  (remove-hook 'emacs-everywhere-init-hooks #'emacs-everywhere-set-frame-position))
+;;(after! emacs-everywhere
+;;  ;; Easier to match with a bspwm rule:
+;;  ;;   bspc rule -a 'Emacs:emacs-everywhere' state=floating sticky=on
+;;  (setq emacs-everywhere-frame-name-format "emacs-anywhere")
+;;
+;;  ;; Leave it to BSPWM to position/size the emacs-everywhere window, rather than
+;;  ;; place it at the cursor position (which could be anywhere).
+;;  (remove-hook 'emacs-everywhere-init-hooks #'emacs-everywhere-set-frame-position))
 
 
 ;;; :email mu4e
