48e2acd751cc58496519a6b28a70ae1db4074045
roshan <roshan.nair@protonmail.com>
2025-08-15 01:16:09 +0530

    custom changes

--- modules/desktop/niri/niri.mod.nix
+++ modules/desktop/niri/niri.mod.nix
@@ -5,14 +5,7 @@
       nixpkgs.overlays = [ niri.overlays.niri ];
       programs.niri.package = pkgs.niri-unstable;
       environment.variables.NIXOS_OZONE_WL = "1";
-      environment.systemPackages = with pkgs; [
-        wl-clipboard
-        wayland-utils
-        libsecret
-        cage
-        gamescope
-        xwayland-satellite-unstable
-      ];
+
       home-shortcut.imports = [
         (
           {
