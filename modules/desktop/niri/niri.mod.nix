f0b38cd29221bb83decda8aa4905c135a734a13e
roshan <roshan.nair@protonmail.com>
2025-08-16 02:19:53 +0530

    custom changes

--- modules/desktop/niri/niri.mod.nix
+++ modules/desktop/niri/niri.mod.nix
@@ -1,9 +1,6 @@
 { config, lib, pkgs, inputs, ... }:
 
 {
-  imports = [
-    ./niri_config_mod.nix
-  ];
   nixpkgs.overlays = [ inputs.niri.overlays.niri ];
   programs.niri = {
     enable = true;
