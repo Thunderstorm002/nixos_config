bc76a74e4eac2f77a5cb93a17afae9c5e2ea1b06
roshan <roshan.nair@protonmail.com>
2025-08-17 14:46:22 +0530

    custom changes

--- home/config/fuzzel.nix
+++ home/config/fuzzel.nix
@@ -1,24 +1,46 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg.configFile."fuzzel/fuzzel.ini".text = ''
-    #fuzzy=yes
-    width=80
+    programs.fuzzel = {
+        enable = true;
+        settings = {
+            main = {
+                width = 80;
+            };
+            colors = {
+                background = "0a0e14ff";
+                text = "b3b1adff";
+                match = "26d3b5ff";
+                selection = "ff9940ff";
+                selection-text = "0a0e14ff";
+                border = "59c2ffff";
+            };
+            border = {
+                width = 1;
+            };
+            dmenu = {
+                mode = "text";
+            };
+        };
+    };
+  #xdg.configFile."fuzzel/fuzzel.ini".text = ''
+  #  #fuzzy=yes
+  #  width=80
 
-    [colors]
-    background=0a0e14ff
-    text=b3b1adff
-    match=26d3b5ff
-    selection=ff9940ff
-    selection-text=0a0e14ff
-    border=59c2ffff
+ ##   [colors]
+  #  background=0a0e14ff
+  #  text=b3b1adff
+  #  match=26d3b5ff
+  #  selection=ff9940ff
+  #  selection-text=0a0e14ff
+  #  border=59c2ffff
 
-    [border]
-    width=1
-    # radius=10
+ ##   [border]
+  #  width=1
+  #  # radius=10
 
-    [dmenu]
-    mode=text  # text|index
-    exit-immediately-if-empty=no
-  '';
+ ##   [dmenu]
+  #  mode=text  # text|index
+  #  exit-immediately-if-empty=no
+  #'';
 }
