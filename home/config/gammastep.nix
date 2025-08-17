04e071e42cb11c43aa19f9b591bbd4b3f79a05cd
roshan <roshan.nair@protonmail.com>
2025-08-18 01:52:07 +0530

    custom changes

--- home/config/gammastep.nix
+++ home/config/gammastep.nix
@@ -1,29 +1,33 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-    services.gammastep = {
-        enable = true;
+  services.gammastep = {
+    enable = true;
 
-        dawnTime = "6:00";
-        duskTime = "18:00";
+    dawnTime = "6:00";
+    duskTime = "18:00";
 
-        provider = "manual";
-        latitude = 12.9;
-        longitude = 77.6;
+    provider = "manual";
+    latitude = 12.9;
+    longitude = 77.6;
 
-        settings = {
-            general = {
-                adjustment-method = "wayland";
+    settings = {
+      general = {
+        adjustment-method = "wayland";
 
-                temp-day = lib.mkForce 5700;
-                temp-night = lib.mkForce 4500;
+        temp-day = lib.mkForce 5700;
+        temp-night = lib.mkForce 4500;
 
-                fade = 1;
+        fade = 1;
 
-                gamma-day = "0.8:0.7:0.8";
-                gamma-night = "0.5";
-            };
-        };
-
+        gamma-day = "0.8:0.7:0.8";
+        gamma-night = "0.5";
+      };
     };
+  };
 }
