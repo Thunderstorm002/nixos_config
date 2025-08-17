0ccad5ce3f63157e44baa12f2c3bbc4b50bbec5b
roshan <roshan.nair@protonmail.com>
2025-08-17 14:48:06 +0530

    custom changes

--- home/config/fuzzel.nix
+++ home/config/fuzzel.nix
@@ -8,12 +8,12 @@
                 width = 80;
             };
             colors = {
-                background = "0a0e14ff";
-                text = "b3b1adff";
-                match = "26d3b5ff";
-                selection = "ff9940ff";
-                selection-text = "0a0e14ff";
-                border = "59c2ffff";
+                background = lib.mkForce "0a0e14ff";
+                text = lib.mkForce "b3b1adff";
+                match = lib.mkForce "26d3b5ff";
+                selection = lib.mkForce "ff9940ff";
+                selection-text = lib.mkForce "0a0e14ff";
+                border = lib.mkForce "59c2ffff";
             };
             border = {
                 width = 1;
